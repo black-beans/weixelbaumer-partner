446dc1ba13c693941c9ce8a7951b58ff7155b4e1
Michael Kessler <netzpirat@flinkfinger.com>
2014-01-16 19:43:01 +0100

    Add initial ken burns effect.

--- source/scripts/application.js
+++ source/scripts/application.js
@@ -4,56 +4,80 @@
  */
 
 function preloadImages() {
-    var images = ['angebot_change_und_projektmanagement.png', 'angebot_coaching_und_training.png', 'angebot_unternehmensentwicklung.png', 'home_unternehmensentwicklung.png', 'home_coaching_und_training.png', 'home_change_und_projektmanagement.png'];
-    for (var image in images) {
-        $('<img />').attr('src', '/images/' + images[image]);
-    }
+  var images = ['angebot_change_und_projektmanagement.png', 'angebot_coaching_und_training.png', 'angebot_unternehmensentwicklung.png', 'home_unternehmensentwicklung.png', 'home_coaching_und_training.png', 'home_change_und_projektmanagement.png'];
+  for (var image in images) {
+    $('<img />').attr('src', '/images/' + images[image]);
+  }
 }
 
 function attachImageSwap(id, image, reset) {
-    $('#' + id).mouseenter(function () {
-        $('#homeMap').attr('src', '/images/' + image);
-    }).mouseleave(function () {
-        $('#homeMap').attr('src', '/images/' + reset);
-    }).touchend(function() {
-      // Empty touch event is needed to fix ImageMap touch
-    });
+  $('#' + id).mouseenter(function () {
+    $('#homeMap').attr('src', '/images/' + image);
+  }).mouseleave(function () {
+    $('#homeMap').attr('src', '/images/' + reset);
+  }).touchend(function() {
+    // Empty touch event is needed to fix ImageMap touch
+  });
 }
 
 function attachTextEffect(id, top, left, words) {
-    window[id + 'Pos'] = 0;
+  window[id + 'Pos'] = 0;
 
-    $('#' + id).mouseenter(function() {
-        showTextEffect(id, words, top, left);
-        window['intervalID'] = setInterval(function() { showTextEffect(id, words, top, left) }, 1000);
-    }).mouseleave(function() {
-            clearInterval(window['intervalID']);
-            $('#text').remove();
-        });
+  $('#' + id).mouseenter(function() {
+    showTextEffect(id, words, top, left);
+    window['intervalID'] = setInterval(function() { showTextEffect(id, words, top, left) }, 1000);
+  }).mouseleave(function() {
+    clearInterval(window['intervalID']);
+    $('#text').remove();
+  });
+}
+
+function isCanvasSupported(){
+  var elem = document.createElement('canvas');
+  return !!(elem.getContext && elem.getContext('2d'));
 }
 
 function showTextEffect(id, words, top, left) {
-    $('#text').hide('puff', 500).remove();
-    $("<div id='text' style='color: white; display: none; position: absolute; top: " + top + "px; left: " + left + "px;' >" + words[window[id + 'Pos']] + '</div>').appendTo($('div.competence')).fadeIn(500);
-    window[id + 'Pos'] = window[id + 'Pos'] == words.length - 1 ? 0 : window[id + 'Pos'] += 1;
+  $('#text').hide('puff', 500).remove();
+  $("<div id='text' style='color: white; display: none; position: absolute; top: " + top + "px; left: " + left + "px;' >" + words[window[id + 'Pos']] + '</div>').appendTo($('div.competence')).fadeIn(500);
+  window[id + 'Pos'] = window[id + 'Pos'] == words.length - 1 ? 0 : window[id + 'Pos'] += 1;
 }
 
 $(document).ready(function () {
-    preloadImages();
+  preloadImages();
 
-    attachImageSwap('angebotchangeundprojektmanagement', 'angebot_change_und_projektmanagement.png', 'angebot.png');
-    attachImageSwap('angebotcoachingundtraining', 'angebot_coaching_und_training.png', 'angebot.png');
-    attachImageSwap('angebotunternehmensentwicklung', 'angebot_unternehmensentwicklung.png', 'angebot.png');
+  if (isCanvasSupported()) {
+    var ken = $('.ken');
 
-    attachImageSwap('homechangeundprojektmanagement', 'home_change_und_projektmanagement.png', 'home.png');
-    attachImageSwap('homecoachingundtraining', 'home_coaching_und_training.png', 'home.png');
-    attachImageSwap('homeunternehmensentwicklung', 'home_unternehmensentwicklung.png', 'home.png');
+    if (ken.length > 0) {
+      ken.after('<canvas class="kenburns"></canvas>');
 
-    $('#homechangeundprojektmanagement').mTip({ alignTo: 'cursor', spacing: 13 });
-    $('#homecoachingundtraining').mTip({ alignTo: 'cursor', spacing: 13 });
-    $('#homeunternehmensentwicklung').mTip({ alignTo: 'cursor', spacing: 13 });
+      $('.kenburns').kenburns({
+        images: [ken.attr('src')],
+        frames_per_second: 24,
+        display_time: 8000,
+        fade_time: 2000,
+        zoom: 1.1
+      });
 
-    attachTextEffect('unternehmen', 110, 158, ['Wirtschaftlichkeit', 'Markt', 'Rechtsform', 'Produkte', 'Strategie', 'Dienstleistungen', 'Branche', 'Hierarchie', 'Standorte', 'Philosophie', 'Struktur', 'Kultur', 'Zielkunde']);
-    attachTextEffect('mensch', 215, 415, ['Unternehmer', 'Werte', 'Führungskraft', 'Kulturkreise', 'Mitarbeiter', 'Erfahrungen', 'Kompetenzen', 'Projektleiter', 'Einstellungen', 'Erwartungen', 'Experte', 'Netzwerke']);
-    attachTextEffect('methodik', 320, 158, ['Instrumente', 'Verständlichkeit', 'Massgeschneidert', 'Hilfsmittel', 'Funktional', 'Wirksamkeit', 'Investition', 'Vorgehensweisen', 'Skalierbarkeit', 'Kompatibilität', 'Aufwand']);
+      ken.remove();
+    }
+  }
+
+
+  attachImageSwap('angebotchangeundprojektmanagement', 'angebot_change_und_projektmanagement.png', 'angebot.png');
+  attachImageSwap('angebotcoachingundtraining', 'angebot_coaching_und_training.png', 'angebot.png');
+  attachImageSwap('angebotunternehmensentwicklung', 'angebot_unternehmensentwicklung.png', 'angebot.png');
+
+  attachImageSwap('homechangeundprojektmanagement', 'home_change_und_projektmanagement.png', 'home.png');
+  attachImageSwap('homecoachingundtraining', 'home_coaching_und_training.png', 'home.png');
+  attachImageSwap('homeunternehmensentwicklung', 'home_unternehmensentwicklung.png', 'home.png');
+
+  $('#homechangeundprojektmanagement').mTip({ alignTo: 'cursor', spacing: 13 });
+  $('#homecoachingundtraining').mTip({ alignTo: 'cursor', spacing: 13 });
+  $('#homeunternehmensentwicklung').mTip({ alignTo: 'cursor', spacing: 13 });
+
+  attachTextEffect('unternehmen', 110, 158, ['Wirtschaftlichkeit', 'Markt', 'Rechtsform', 'Produkte', 'Strategie', 'Dienstleistungen', 'Branche', 'Hierarchie', 'Standorte', 'Philosophie', 'Struktur', 'Kultur', 'Zielkunde']);
+  attachTextEffect('mensch', 215, 415, ['Unternehmer', 'Werte', 'Führungskraft', 'Kulturkreise', 'Mitarbeiter', 'Erfahrungen', 'Kompetenzen', 'Projektleiter', 'Einstellungen', 'Erwartungen', 'Experte', 'Netzwerke']);
+  attachTextEffect('methodik', 320, 158, ['Instrumente', 'Verständlichkeit', 'Massgeschneidert', 'Hilfsmittel', 'Funktional', 'Wirksamkeit', 'Investition', 'Vorgehensweisen', 'Skalierbarkeit', 'Kompatibilität', 'Aufwand']);
 });
